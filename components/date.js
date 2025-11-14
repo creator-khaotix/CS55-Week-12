7f9c715d361edd08117088b27bfd4c24c5d9b563
Phil Weyhe <pweyhe@bearcubs.santarosa.edu>
2025-11-14 09:40:37 -0800

    Fixed date parsing for WordPress date format (still using got)

--- components/date.js
+++ components/date.js
@@ -1,6 +1,10 @@
 import { parseISO, format } from 'date-fns';
  
 export default function Date({ dateString }) {
-  const date = parseISO(dateString);
-  return <time dateTime={dateString}>{format(date, 'LLLL d, yyyy')}</time>;
+  // Convert WordPress date format to ISO-8601 format
+  // WordPress returns: "2025-11-05 09:19:03"
+  // ISO-8601 needs: "2025-11-05T09:19:03"
+  const isoDateString = dateString.replace(' ', 'T');
+  const date = parseISO(isoDateString);
+  return <time dateTime={isoDateString}>{format(date, 'LLLL d, yyyy')}</time>;
 }
